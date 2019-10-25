2c2995a14b181d5b85b5ddfdd6d8d76012fbdad6
L2zz <ulla4571@gmail.com>
2019-10-25 22:14:36 +0900

    [Module] Loader for opencv
    Async initialization of opencv to call library,
    after loading is completed.
    
    Signed-off-by: L2zz <ulla4571@gmail.com>

--- app/src/main/java/edu/skku/color_recommender/MainActivity.java
+++ app/src/main/java/edu/skku/color_recommender/MainActivity.java
@@ -5,9 +5,36 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("opencv_java3");
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        } else {
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 }
