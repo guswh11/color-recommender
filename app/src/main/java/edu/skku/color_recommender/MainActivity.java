4fe1ff7fd755cf9c7361374df3c4f586a0a3e4dd
L2zz <ulla4571@gmail.com>
2019-10-25 15:37:30 +0900

    [Init] Init project
    Initialize project and import module named object-detection.
    
    Signed-off-by: L2zz <ulla4571@gmail.com>

---
+++ app/src/main/java/edu/skku/color_recommender/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.skku.color_recommender;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
