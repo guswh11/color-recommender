fa800029ced4fbe8f2cfc0c7bf047b01f85aaf9c
Hyunjin <guswh11@naver.com>
2019-11-04 21:04:17 +0900

    Modify result-acitivty layout
    Add viewpager and its indicator.
    
    Signed-off-by: Hyunjin <guswh11@naver.com>

--- app/src/main/java/edu/skku/color_recommender/ResultActivity.java
+++ app/src/main/java/edu/skku/color_recommender/ResultActivity.java
@@ -1,14 +1,24 @@
 package edu.skku.color_recommender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class ResultActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        tabLayout.setupWithViewPager(viewPager, true); 
     }
 }
