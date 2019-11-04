c0215fe66c593afb0fb2fbc1aece45b44ed25486
Hyunjin <guswh11@naver.com>
2019-11-04 16:03:22 +0900

    Update TempActivity.java
    Delete screen-capturing func.
    
    Signed-off-by: Hyunjin <guswh11@naver.com>

--- app/src/main/java/edu/skku/color_recommender/TempActivity.java
+++ app/src/main/java/edu/skku/color_recommender/TempActivity.java
@@ -1,7 +1,5 @@
 package edu.skku.color_recommender;
 
-import android.annotation.SuppressLint;
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,11 +11,6 @@ import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.RectF;
 import android.graphics.Typeface;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.params.StreamConfigurationMap;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.Image;
@@ -32,23 +25,18 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.SystemClock;
 import android.os.Trace;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.Size;
 import android.util.TypedValue;
 import android.view.Display;
-import android.view.OrientationEventListener;
 import android.view.Surface;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.tensorflow.lite.examples.detection.CameraActivity;
-import org.tensorflow.lite.examples.detection.CameraConnectionFragment;
-import org.tensorflow.lite.examples.detection.LegacyCameraConnectionFragment;
 import org.tensorflow.lite.examples.detection.customview.OverlayView;
 import org.tensorflow.lite.examples.detection.env.BorderedText;
 import org.tensorflow.lite.examples.detection.env.ImageUtils;
@@ -57,33 +45,13 @@ import org.tensorflow.lite.examples.detection.tflite.Classifier;
 import org.tensorflow.lite.examples.detection.tflite.TFLiteObjectDetectionAPIModel;
 import org.tensorflow.lite.examples.detection.tracking.MultiBoxTracker;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.List;
 
 public class TempActivity extends AppCompatActivity{
-    private static final String TAG = "ScreenTestCapture";
-    private static final int REQUEST_CODE = 100;
-    private static String STORE_DIRECTORY;
-    private static int IMAGES_PRODUCED;
-    private static final String SCREENCAP_NAME = "screencap";
-    private static final int VIRTUAL_DISPLAY_FLAGS = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY | DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC;
-    private static MediaProjection sMediaProjection;
-
-    private MediaProjectionManager mProjectionManager;
-    private ImageReader mImageReader;
-    private Handler mHandler;
-    private Display mDisplay;
-    private VirtualDisplay mVirtualDisplay;
-    private int mDensity;
     private int mWidth;
     private int mHeight;
-    private int mRotation;
-    private OrientationChangeCallback mOrientationChangeCallback;
 
     private ImageView pictureView;
     private Uri fileUri;
@@ -99,90 +67,6 @@ public class TempActivity extends AppCompatActivity{
     private boolean debug = false;
     private Handler handler;
     private HandlerThread handlerThread;
-
-    private class ImageAvailableListener implements ImageReader.OnImageAvailableListener {
-        Image image = null;
-        FileOutputStream fos = null;
-        Bitmap bitmap = null;
-
-        @Override
-        public void onImageAvailable(ImageReader imageReader) {
-            try {
-                // Initialize the storage bitmaps once when the resolution is known.
-                if (rgbBytes == null) {
-                    previewHeight = mHeight;
-                    previewWidth = mWidth;
-                    rgbBytes = new int[previewWidth * previewHeight];
-                    onPreviewSizeChosen(new Size(previewWidth, previewHeight), 90);
-                }
-            } catch (final Exception e) {
-                LOGGER.e(e, "Exception!");
-                return;
-            }
-
-            try {
-                final Image image = mImageReader.acquireLatestImage();
-
-                if (image == null) {
-                    Log.d("image","no image");
-                    return;
-                }
-
-                if (isProcessingFrame) {
-                    image.close();
-                    return;
-                }
-                Log.d("image","여기까지3");
-                isProcessingFrame = true;
-                Trace.beginSection("imageAvailable");
-                final Image.Plane[] planes = image.getPlanes();
-                //fillBytes(planes, yuvBytes);
-                yRowStride = planes[0].getRowStride();
-                //final int uvRowStride = planes[1].getRowStride();
-                //final int uvPixelStride = planes[1].getPixelStride();
-
-                ByteBuffer buffer = planes[0].getBuffer();
-                int pixelStride = planes[0].getPixelStride();
-                int rowStride = planes[0].getRowStride();
-                int rowPadding = rowStride - pixelStride * mWidth;
-
-                bitmap = Bitmap.createBitmap(mWidth + rowPadding / pixelStride, mHeight, Bitmap.Config.ARGB_8888);
-                bitmap.copyPixelsFromBuffer(buffer);
-
-                int[] mIntArr = new int[mWidth * mHeight];
-                bitmap.getPixels(mIntArr, 0, mWidth, 0, 0, mWidth, mHeight);
-                final byte[] dest = encodeYUV420SP(mIntArr, mWidth, mHeight);
-
-                Log.d("image_byte",new Integer(dest.length).toString());
-                Log.d("image_byte", new Integer(mWidth).toString()+","+new Integer(mHeight).toString());
-                Log.d("image_byte_size", new Integer(ImageUtils.getYUVByteSize(mWidth, mHeight)).toString());
-                imageConverter =
-                        new Runnable() {
-                            @Override
-                            public void run() {
-                                ImageUtils.convertYUV420SPToARGB8888(dest, mWidth, mHeight, rgbBytes);
-                            }
-                        };
-                Log.d("image","여기까지2");
-                postInferenceCallback =
-                        new Runnable() {
-                            @Override
-                            public void run() {
-                                image.close();
-                                isProcessingFrame = false;
-                            }
-                        };
-                IMAGES_PRODUCED++;
-                processImage();
-                Log.d("image","여기까지");
-            } catch (final Exception e) {
-                e.printStackTrace();
-                Trace.endSection();
-                return;
-            }
-            Trace.endSection();
-        }
-    }
 
     public byte[] encodeYUV420SP(int[] argb, int width, int height){
         final int frameSize = width * height;
@@ -221,47 +105,6 @@ public class TempActivity extends AppCompatActivity{
         return yuv420sp;
     }
 
-    //화면 rotation 시 사용됨
-    private class OrientationChangeCallback extends OrientationEventListener {
-        public OrientationChangeCallback(Context context) {
-            super(context);
-        }
-
-        @Override
-        public void onOrientationChanged(int orientation) {
-            synchronized (this) {
-                final int rotation = mDisplay.getRotation();
-
-                if (rotation != mRotation) {
-                    mRotation = rotation;
-                    try {
-                        if (mVirtualDisplay != null) mVirtualDisplay.release();
-                        if (mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
-
-                        createVirtualDisplay();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-
-    private class MediaProjectionStopCallback extends MediaProjection.Callback {
-        @Override
-        public void onStop() {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if (mVirtualDisplay != null) mVirtualDisplay.release();
-                    if (mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
-                    if (mOrientationChangeCallback != null) mOrientationChangeCallback.disable();
-                    sMediaProjection.unregisterCallback(MediaProjectionStopCallback.this);
-                }
-            });
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -271,131 +114,39 @@ public class TempActivity extends AppCompatActivity{
         pictureView = findViewById(R.id.pictureView);
         pictureView.setImageURI(fileUri);
 
-        Log.d("imae_", new Integer(pictureView.getWidth()).toString());
-
-        mProjectionManager = (MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
-
-/*
-        Button startButton = (Button)findViewById(R.id.startButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startProjection();
-            }
-        });
-
-        Button stopButton = (Button)findViewById(R.id.stopButton);
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                stopProjection();
-            }
-        });
-
-
- */
-        new Thread() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                mHandler = new Handler();
-                Looper.loop();
-            }
-        }.start();
-
-        startProjection();
-    }
-
-    @Override
-    public synchronized void onResume() {
-        LOGGER.d("onResume " + this);
-        super.onResume();
-
+        Display display = this.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        mWidth = size.x;
+        mHeight = size.y;
         handlerThread = new HandlerThread("inference");
         handlerThread.start();
         handler = new Handler(handlerThread.getLooper());
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            sMediaProjection = mProjectionManager.getMediaProjection(resultCode, data);
+        onPreviewSizeChosen(new Size(mWidth, mHeight), 90);
+        byte[] dest;
+        Bitmap bitmap;
+        int[] mIntArr = new int[mWidth * mHeight];
+        rgbBytes = new int[previewWidth * previewHeight];
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fileUri);
+            bitmap.getPixels(mIntArr, 0, mWidth, 0, 0, mWidth, mHeight);
 
-            if (sMediaProjection != null) {
-                //캡쳐한 화면 이미지를 저장
-                STORE_DIRECTORY = Environment.getExternalStorageDirectory() + "/data/capturetest/";
-                File storeDirectory = new File(STORE_DIRECTORY);
-                if (!storeDirectory.exists()) {
-                    boolean success = storeDirectory.mkdirs();
-                    if (!success) {
-                        return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dest = encodeYUV420SP(mIntArr, mWidth, mHeight);
+        imageConverter =
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageUtils.convertYUV420SPToARGB8888(dest, mWidth, mHeight, rgbBytes);
                     }
-                }
-            }
-
-            DisplayMetrics metrics = getResources().getDisplayMetrics();
-            mDensity = metrics.densityDpi;
-            mDisplay = getWindowManager().getDefaultDisplay();
-
-            createVirtualDisplay();
-
-            mOrientationChangeCallback = new OrientationChangeCallback(this);
-            if (mOrientationChangeCallback.canDetectOrientation()) {
-                mOrientationChangeCallback.enable();
-            }
-
-            sMediaProjection.registerCallback(new MediaProjectionStopCallback(), mHandler);
-        }
+                };
+        processImage();
     }
 
-    private void startProjection() {
-        startActivityForResult(mProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
-    }
-
-    private void stopProjection() {
-        //projection을 종료
-        //mediaprojection callback의 onstop method가 호출됨
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                if (sMediaProjection != null) {
-                    sMediaProjection.stop();
-                }
-            }
-        });
-    }
-
-    //가상 디스플레이 생성
-    private void createVirtualDisplay() {
-        Point size = new Point();
-        mDisplay.getSize(size);
-        mWidth = size.x;
-        mHeight = size.y;
-
-        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 5);
-        mVirtualDisplay = sMediaProjection.createVirtualDisplay(SCREENCAP_NAME, mWidth, mHeight, mDensity, VIRTUAL_DISPLAY_FLAGS, mImageReader.getSurface(), null, mHandler);
-        mImageReader.setOnImageAvailableListener(new ImageAvailableListener(), mHandler);
-    }
-
-    protected void readyForNextImage() {
-        if (postInferenceCallback != null) {
-            postInferenceCallback.run();
-        }
-    }
-
-    protected void fillBytes(final Image.Plane[] planes, final byte[][] yuvBytes) {
-        // Because of the variable row stride it's not possible to know in
-        // advance the actual necessary dimensions of the yuv planes.
-        Log.d("image",new Integer(planes.length).toString());
-        for (int i = 0; i < planes.length; ++i) {
-            final ByteBuffer buffer = planes[i].getBuffer();
-            if (yuvBytes[i] == null) {
-                yuvBytes[i] = new byte[buffer.capacity()];
-            }
-            buffer.get(yuvBytes[i]);
-        }
-    }
 
     public boolean isDebug() {
         return debug;
@@ -532,17 +283,7 @@ public class TempActivity extends AppCompatActivity{
         final long currTimestamp = timestamp;
         //trackingOverlay.postInvalidate();
 
-        // No mutex needed as this method is not reentrant.
-        if (computingDetection) {
-            readyForNextImage();
-            return;
-        }
-        computingDetection = true;
-        LOGGER.i("Preparing image " + currTimestamp + " for detection in bg thread.");
-
         rgbFrameBitmap.setPixels(getRgbBytes(), 0, previewWidth, 0, 0, previewWidth, previewHeight);
-
-        readyForNextImage();
 
         final Canvas canvas = new Canvas(croppedBitmap);
         canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
@@ -551,7 +292,6 @@ public class TempActivity extends AppCompatActivity{
             ImageUtils.saveBitmap(croppedBitmap);
         }
 
-        Log.d("process_image","여기까지");
         runInBackground(
                 new Runnable() {
                     @Override
