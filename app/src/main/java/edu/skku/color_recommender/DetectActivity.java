5b3c6c0999563c21ed614f7ec06d023095e7c6cc
Hyunjin <guswh11@naver.com>
2019-11-05 15:52:45 +0900

    Modify min-confidence-score
    Change minimum confidence score to 0.4f.
    
    Signed-off-by: Hyunjin <guswh11@naver.com>

--- app/src/main/java/edu/skku/color_recommender/DetectActivity.java
+++ app/src/main/java/edu/skku/color_recommender/DetectActivity.java
@@ -195,7 +195,7 @@ public class DetectActivity extends AppCompatActivity{
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
     private static final DetectorMode MODE = DetectorMode.TF_OD_API;
     // Minimum detection confidence to track a detection.
-    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
+    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.4f;
     private static final boolean MAINTAIN_ASPECT = true;
     private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
     private static final boolean SAVE_PREVIEW_BITMAP = false;
