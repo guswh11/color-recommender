a14607897125fcea68ddee5841fadd7790e1c7ff
L2zz <ulla4571@gmail.com>
2019-11-06 00:42:28 +0900

    [Example] Color extractor example
    Get bitmap in drawable and get main  color
    using color extractor.
    
    Signed-off-by: L2zz <ulla4571@gmail.com>

--- app/src/main/java/edu/skku/color_recommender/MainActivity.java
+++ app/src/main/java/edu/skku/color_recommender/MainActivity.java
@@ -1,9 +1,24 @@
 package edu.skku.color_recommender;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ColorExtractor ce;
+    private ImageView iv;
 
     static {
         System.loadLibrary("opencv_java3");
@@ -14,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         public void onManagerConnected(int status) {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
+                    Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.blue)).getBitmap();
+                    ArrayList<Color> result = ce.extractColor(bitmap);
+                    iv.setBackgroundColor(Color.rgb(result.get(0).red(), result.get(0).green(), result.get(0).blue()));
+                    Log.d("COLOR_EXTRACT", "" + result.get(0));
+                    Log.d("COLOR_EXTRACT", "" + result.get(1));
+                    Log.d("COLOR_EXTRACT", "" + result.get(2));
+                    Log.d("COLOR_EXTRACT", ce.getColorName(result.get(0)));
                     break;
                 default:
                     super.onManagerConnected(status);
@@ -26,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.main_iv);
+        ce = new ColorExtractor();
     }
 
     @Override
