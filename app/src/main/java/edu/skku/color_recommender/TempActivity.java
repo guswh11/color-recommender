09dcd39152b4685fcd632819e0102c6112678fad
Hyunjin <guswh11@naver.com>
2019-11-05 13:26:41 +0900

    Update TempActivity.java
    Delete unused lines.
    
    Signed-off-by: Hyunjin <guswh11@naver.com>

--- app/src/main/java/edu/skku/color_recommender/TempActivity.java
+++ app/src/main/java/edu/skku/color_recommender/TempActivity.java
@@ -45,52 +45,10 @@ public class TempActivity extends AppCompatActivity{
 
     protected int previewWidth = 0;
     protected int previewHeight = 0;
-    private byte[][] yuvBytes = new byte[3][];
     private int[] rgbBytes = null;
-    private int yRowStride;
-    private boolean isProcessingFrame = false;
-    private Runnable postInferenceCallback;
-    private Runnable imageConverter;
     private boolean debug = false;
     private Handler handler;
     private HandlerThread handlerThread;
-
-    public byte[] encodeYUV420SP(int[] argb, int width, int height){
-        final int frameSize = width * height;
-
-        int yIndex = 0;
-        int uvIndex = frameSize;
-
-        byte[] yuv420sp = new byte[ImageUtils.getYUVByteSize(mWidth, mHeight)];
-        int a, R, G, B, Y, U, V;
-        int index = 0;
-        for (int j = 0; j < height; j++) {
-            for (int i = 0; i < width; i++) {
-
-                a = (argb[index] & 0xff000000) >> 24; // a is not used obviously
-                R = (argb[index] & 0xff0000) >> 16;
-                G = (argb[index] & 0xff00) >> 8;
-                B = (argb[index] & 0xff) >> 0;
-
-                // well known RGB to YUV algorithm
-                Y = ( (  66 * R + 129 * G +  25 * B + 128) >> 8) +  16;
-                U = ( ( -38 * R -  74 * G + 112 * B + 128) >> 8) + 128;
-                V = ( ( 112 * R -  94 * G -  18 * B + 128) >> 8) + 128;
-
-                // NV21 has a plane of Y and interleaved planes of VU each sampled by a factor of 2
-                //    meaning for every 4 Y pixels there are 1 V and 1 U.  Note the sampling is every other
-                //    pixel AND every other scanline.
-                yuv420sp[yIndex++] = (byte) ((Y < 0) ? 0 : ((Y > 255) ? 255 : Y));
-                if (j % 2 == 0 && index % 2 == 0) {
-                    yuv420sp[uvIndex++] = (byte)((V<0) ? 0 : ((V > 255) ? 255 : V));
-                    yuv420sp[uvIndex++] = (byte)((U<0) ? 0 : ((U > 255) ? 255 : U));
-                }
-
-                index ++;
-            }
-        }
-        return yuv420sp;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -195,11 +153,6 @@ public class TempActivity extends AppCompatActivity{
             default:
                 return 0;
         }
-    }
-
-    protected int[] getRgbBytes() {
-        imageConverter.run();
-        return rgbBytes;
     }
 
     protected synchronized void runInBackground(final Runnable r) {
@@ -309,13 +262,6 @@ public class TempActivity extends AppCompatActivity{
 
     }
 
-    protected int getLayoutId() {
-        return org.tensorflow.lite.examples.detection.R.layout.camera_connection_fragment_tracking;
-    }
-
-    protected Size getDesiredPreviewFrameSize() {
-        return DESIRED_PREVIEW_SIZE;
-    }
     // Which detection model to use: by default uses Tensorflow Object Detection API frozen
     // checkpoints.
     private enum DetectorMode {
